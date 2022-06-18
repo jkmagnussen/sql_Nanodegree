bc4ca68a0cf79136038253a94be2a72cb96af581
JKMagnussen <jkmagnussen@icloud.com>
2022-06-18 03:05:33 +0100

    q1/8 completed

--- parch-and-posey/SQL_Joins.sql
+++ parch-and-posey/SQL_Joins.sql
@@ -151,6 +151,16 @@ SLIDE 19
 
 Answer: 
 
+SELECT r.name AS region_name, 
+s.name AS sales_reps_name, 
+a.name AS account_name
+FROM sales_reps s
+LEFT JOIN region r
+ON s.region_id = r.id
+LEFT JOIN accounts a
+ON a.sales_rep_id = s.id 
+WHERE r.name = 'Midwest'
+ORDER BY a.name;
 
 
-2.Provide a table that provides the region for each sales_rep along with their associated accounts. This time only for accounts where the sales rep has a first name starting with S and in the Midwest region. Your final table should include three columns: the region name, the sales rep name, and the account name. Sort the accounts alphabetically (A-Z) according to the account name..
+2.Provide a table that provides the region for each sales_rep along with their associated accounts. This time only for accounts where the sales rep has a first name starting with S and in the Midwest region. Your final table should include three columns: the region name, the sales rep name, and the account name. Sort the accounts alphabetically (A-Z) according to the account name.
