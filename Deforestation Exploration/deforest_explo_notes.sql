55ef3a486c5918011c5920989fb1f2684498a346
JKMagnussen <jkmagnussen@icloud.com>
2022-07-05 17:36:21 +0100

    new

--- Deforestation Exploration/deforest_explo_notes.sql
+++ Deforestation Exploration/deforest_explo_notes.sql
@@ -14,33 +14,51 @@ CREATE VIEW forestation AS SELECT f.country_code AS country_code, f.country_name
 
 a. What was the total forest area (in sq km) of the world in 1990? Please keep in mind that you can use the country record denoted as “World" in the region table.
 
-       SELECT SUM(forest_area_sqkm) AS total_area FROM forest_area WHERE year = 1990;
+       SELECT *
+       FROM forestation 
+       WHERE country_name = 'World' 
+       AND year = 1990;
 
-       OUTPUT: 82016472.036028
+       OUTPUT: 41282694.9
 
 b. What was the total forest area (in sq km) of the world in 2016? Please keep in mind that you can use the country record in the table is denoted as “World.”
 
-       SELECT SUM(forest_area_sqkm) AS total_area FROM forest_area WHERE year = 2016;
+       SELECT *
+       FROM forestation 
+       WHERE country_name = 'World' 
+       AND year = 2016;
 
-       OUTPUT: 79825433.9505107
+       OUTPUT: 39958245.9
 
 c. What was the change (in sq km) in the forest area of the world from 1990 to 2016?
 
-       SELECT (SELECT SUM(forest_area_sqkm) FROM forest_area WHERE year = 1990) - (SELECT SUM(forest_area_sqkm) FROM forest_area WHERE year = 2016) AS difference;
+       SELECT (SELECT forest_area_sq_km
+       FROM forestation 
+       WHERE country_name = 'World' 
+       AND year = 1990) - (SELECT forest_area_sq_km
+       FROM forestation 
+       WHERE country_name = 'World' 
+       AND year = 2016) AS difference;
       
-       OUTPUT: 2191038.08551738
+       OUTPUT: 1324449
 
 d. What was the percent change in forest area of the world between 1990 and 2016?
 
-       SELECT (1-((SELECT SUM(forest_area_sqkm) FROM forest_area WHERE year = 2016) / (SELECT SUM(forest_area_sqkm) FROM forest_area WHERE year = 1990)))*100 AS percentage;
+       SELECT (1-((SELECT forest_area_sq_km
+       FROM forestation 
+       WHERE country_name = 'World' 
+       AND year = 2016) / (SELECT forest_area_sq_km
+       FROM forestation 
+       WHERE country_name = 'World' 
+       AND year = 1990)))*100 AS percentage;
 
-       OUTPUT: 2.67146102621301
+       OUTPUT: 3.20824258980245
 
 e. If you compare the amount of forest area lost between 1990 and 2016, to which country's total area in 2016 is it closest to?
 
-       SELECT * FROM forest_area WHERE forest_area_sqkm <= 2191038.08551738 AND year = 2016 ORDER BY forest_area_sqkm DESC LIMIT 1;
+       SELECT * FROM forest_area WHERE forest_area_sqkm <= 1324449 AND year = 2016 ORDER BY forest_area_sqkm DESC LIMIT 1;
 
-       OUTPUT: 2098635 - China
+       OUTPUT: 1250590 - Australia
 
 -------------------------------------------------------------------
 PART 2 - REGIONAL OUTLOOK
